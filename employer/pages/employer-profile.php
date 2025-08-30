ed26f50fda71efe890f2f65f2e965e8aaceffac0
jstncrz <cruzjustin129813@gmail.com>
2025-08-31 00:22:01 +0800

    application list update
    fetch on database

--- employer/pages/employer-profile.php
+++ employer/pages/employer-profile.php
@@ -312,9 +312,9 @@ $baseURL = "http://localhost/CAPSTONE/CAPSTONE-PROJECT/";
   <div class="modal-content">
     <span id="closeModal">&times;</span>
     <iframe id="docFrame" src="" width="100%" height="500px" style="display:none;"></iframe>
-    <img id="docImage" src="" style="max-width:100%; height:auto; display:none;"/>
+    <img id="docImage" src="" style="max-width:50%; height:auto; display:none;"/>
   </div>
-</div>
+  </div>
   <script>
     const accountData = <?php echo $accountJson; ?>;
 
