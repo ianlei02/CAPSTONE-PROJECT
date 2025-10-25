940f63ca1dabadf1549439212db24aa62f2e4afd
ianlei02 <ianleicastillo.basc@gmail.com>
2025-10-25 22:34:10 +0800

    new db name

--- connection/dbcon.php
+++ connection/dbcon.php
@@ -1,7 +1,7 @@
 <?php
     
     $conn = new mysqli('localhost', 'root', '', 'peso_3c');
-    // $conn = new mysqli('localhost', 'u584845886_root', 'PESO4c2025', 'u584845886_peso_4c');
+        // $conn = new mysqli('localhost', 'u686565759_peso_admin', 'PESO4c2025', 'u686565759_peso_smb');
     if($conn){
         // echo "Connection Successful"
     }
